c83ec74fa4ba4ef2c9aa1a86b8e717fe75f1645f
Gufi015 <lucras015@hotmail.com>
2018-07-07 00:05:53 -0500

    creacion del controlador main

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,13 @@
 
 $.btnLogin.addEventListener('click', function(e){
-	alert('Bienvenido ' + $.txtUser.value);
+	//alert('Bienvenido ' + $.txtUser.value);
+	if($.txtUser.value =="admin" && $.txtPass.value=="admin"){
+		var main = Alloy.createController('main').getView();
+		main.open();
+	}else{
+		var sing = Alloy.createController('sing').getView();
+		sing.open();
+	}
 	Titanium.API.info('presionaste el boton');
 });
 
