783d724b4394c855b28f97dff200b66b0eb19815
Gufi015 <lucras015@hotmail.com>
2018-07-23 12:49:42 -0500

    Funcion Consulta

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -12,4 +12,22 @@ $.btnMain.addEventListener('click', function(e){
 			alert('error' + JSON.stringify(e));
 		}
 	});*/
+
+	Cloud.Users.queryUsers({
+		page: 1,
+		per_page: 10,
+		where:{}
+	},function(e){
+		if(e.succes){
+			alert('Consulta:\n'+ 'Count: ' + e.users.length);
+			for (var i = 0;  i < e.users.length; i++){ 
+				var user = users[i];
+				alert('ID: ' + user.id + '\n' +
+					'FIRST NAME: ' + user.first_name + '\n' +
+					'LAST_NAME: ' +user.last_name);
+			}
+		}else{
+			alert('Error \n' +((e.error && e.message)|| JSON.stringify(e)));
+		}
+	});
 });
