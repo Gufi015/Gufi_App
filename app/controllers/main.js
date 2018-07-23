3cbe9e743c4ef99d1bf30082ab4f46229934a1b0
Gufi015 <lucras015@hotmail.com>
2018-07-23 11:39:28 -0500

    Nuevos cambios

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,7 +1,15 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
-
+var Cloud = require('ti.cloud');
 
 $.btnMain.addEventListener('click', function(e){
-	alert('Bienvendo');
+	//alert('Bienvendo');
+	Cloud.Users.showMe(function(e){
+		if(e.succes){
+			var user = e.users[0];
+			alert('SUCCES \N'+ user.id +'USER_ID');
+		}else{
+			alert('error');
+		}	
+	});
 });
