5ad0dd4282ecb9efdb840111df645e2aa95971aa
Gufi015 <lucras015@hotmail.com>
2018-07-24 13:45:59 -0500

    Correciones ACS photos

--- app/controllers/forgotPassword.js
+++ app/controllers/forgotPassword.js
@@ -1,22 +1,36 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
 var Cloud = require('ti.cloud');
-
+var image;
 $.btnPhoto.addEventListener('click', function (e) {
-    Cloud.Photos.create({
-        photo: Titanium.Filesystem.getFile('/images/user.png'),
-    }, function (e) {
-        if (e.success) {
-            var photo = e.photos[0];
-            alert('Success:\n' +
-                'ID:' + photo.id + '\n' +
-                'FILE_NAME' + photo.filename + '\n' +
-                'SIZE' + photo.size,
-                'UPDATE_AT:' + photo.updated_at
-            );
-        } else {
-            alert('Error:\n' +
-                (e.error && e.message) || JSON.stringify(e));
-        }
+    Titanium.Media.openPhotoGallery({
+        success: function (e) {
+            if (e.mediaType == Ti.Media.MEDIA_TYPE_PHOTO) {
+                image = e.media;
+                alert(image);
+                Cloud.Photos.create({
+                    photo: image,
+                }, function (e) {
+                    if (e.success) {
+                        var photo = e.photos[0];
+                        alert('Success:\n' +
+                            'ID:' + photo.id + '\n' +
+                            'FILE_NAME' + photo.filename + '\n' +
+                            'SIZE' + photo.size,
+                            'UPDATE_AT:' + photo.updated_at
+                        );
+                    } else {
+                        alert('Error:\n' +
+                            (e.error && e.message) || JSON.stringify(e));
+                    }
+                });
+            }
+        },
+        cancel: function (e) {
+        },
+        error: function(err){
+        	alert('Error'+ err);
+        },
+        mediaTypes:[Ti.Media.MEDIA_TYPE_PHOTO]
     });
 });
