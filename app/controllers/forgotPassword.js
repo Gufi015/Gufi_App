b71861f2fa02d01d5391e1d618a2c3f4999d19b0
Gufi015 <lucras015@hotmail.com>
2018-07-24 16:43:10 -0500

    Correciones

--- app/controllers/forgotPassword.js
+++ app/controllers/forgotPassword.js
@@ -1,15 +1,12 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
 var Cloud = require('ti.cloud');
-//var image;
+var image;
 $.btnPhoto.addEventListener('click', function (e) {
-	var image;
     Titanium.Media.openPhotoGallery({
         success: function (e) {
             if (e.mediaType == Titanium.Media.MEDIA_TYPE_PHOTO) {
-                image = e.media;
-                //alert(image);
-                
+                image = e.mediaType;
                 Cloud.Photos.create({
                     photo: image
                 }, function (e) {
@@ -32,8 +29,7 @@ $.btnPhoto.addEventListener('click', function (e) {
         },
         error: function(err){
         	alert('Error'+ err);
-        },
-        mediaTypes:[Ti.Media.MEDIA_TYPE_PHOTO]
+        }
     });
 });
 
